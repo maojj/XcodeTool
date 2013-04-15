18e7aa2bfed2fe730fa79f72f702f7963b04c507
maojj <maojj@rd.netease.com>
2013-04-15 11:33:34 +0800

    MOD: modify codesnippets

--- setup_file_templates.sh
+++ setup_file_templates.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
-mkdir -p ~/Library/Xcode/Templates/File\ Templates.bak
-mv ~/Library/Xcode/Templates/File\ Templates ~/Library/Xcode/Templates/File\ Templates.bak
+
+DES_HOME=~/Library/Developer/Xcode
+
+cd $DES_HOME
+DES_HOME=`pwd`
+mkdir Templates
+cd Templates
+mkdir File\ Templates.bak
+mv File\ Templates File\ Templates.bak
 
 SRC_HOME=`pwd`
-ln -s ${SRC_HOME}/File\ Templates ~/Library/Xcode/Templates/File\ Templates
+ln -s ${SRC_HOME}/File\ Templates ~/Library/Developer/Xcode/Templates/File\ Templates
 echo "done"
 
